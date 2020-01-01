210988f5eca4b193d35dff1d7d0808bd97e33771
ashis1upreti <59400271+ashis1upreti@users.noreply.github.com>
2019-12-31 20:06:21 -0600

    Update firework_types.js

--- Just4U/firework_types.js
+++ Just4U/firework_types.js
@@ -3,12 +3,6 @@ alert('Are You Ready???!!!!!!');
 alert('Firstly, I want to let you know that words cannot describe how awesome you are just awesome');
 alert('You are indeed an angel God send to me, I always thank God for creating you!');
 alert('This is just for you from the buttom of my heart..');
-alert('You might need a headphone though😀😀');
-alert('1.2...');
-alert('Go!!');
-
-
-
 
 window.onload = function(){
 
